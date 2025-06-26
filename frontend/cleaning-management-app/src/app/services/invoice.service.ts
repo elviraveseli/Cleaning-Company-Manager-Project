5ce3aecbe4d1178fa98c021f8db60e92005aad84
Fjona-Braha <fjona.braha@umib.net>
2025-06-26 12:09:57 +0200

    Update invoice.service.ts

--- frontend/cleaning-management-app/src/app/services/invoice.service.ts
+++ frontend/cleaning-management-app/src/app/services/invoice.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { 
   Invoice, 
   InvoiceListQuery, 
