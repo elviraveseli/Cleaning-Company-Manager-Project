ef2e6591a5cbfb5d87d48083f040590f7dbca659
elviraveseli <124493821+elviraveseli@users.noreply.github.com>
2025-12-29 11:52:07 +0100

    Update scheduleController.js

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -6,7 +6,7 @@ const Customer = require("../models/Customer");
 const Object = require("../models/Object");
 
 // Get all scheduless
-exports.getSchedules = asyncnn (req, res) => {
+exports.getSchedules = asyncn(req, res) => {
   try {
    page = parseInt(req.query.page) || 1;
    limit = parseInt(req.query.limit) || 20;
