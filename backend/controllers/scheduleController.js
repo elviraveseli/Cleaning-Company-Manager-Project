f84a2bc11746fc824a06f3eef8a9f3df80048c09
elviraveseli <124493821+elviraveseli@users.noreply.github.com>
2025-12-29 13:24:55 +0100

    Update scheduleController.js

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -4,7 +4,7 @@ const CustomerContract = require("../models/CustomerContract");
 const Invoice = require("../models/Invoice");
 const Customer = require("../models/Customer");
 const Object = require("../models/Object");
-//test1
+//test12
 
 // Get all scheduless
 exports.getSchedules = asssa(req, res) => {
