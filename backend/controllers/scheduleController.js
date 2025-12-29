e697a06881fa346d4c3ae95623d53cf92dbdca43
elviraveseli <124493821+elviraveseli@users.noreply.github.com>
2025-12-29 11:58:26 +0100

    Fix syntax error in getSchedules function

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -6,7 +6,7 @@ const Customer = require("../models/Customer");
 const Object = require("../models/Object");
 
 // Get all scheduless
-exports.getSchedules = asyncnj(req, res) => {
+exports.getSchedules = async (req, res) => {
   try {
    page = parseInt(req.query.page) || 1;
    limit = parseInt(req.query.limit) || 20;
