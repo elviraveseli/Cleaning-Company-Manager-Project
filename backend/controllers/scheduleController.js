cb80a28b7c10011e950fb9b7a8dc47b0c74b1818
elviraveseli <124493821+elviraveseli@users.noreply.github.com>
2025-12-29 12:02:56 +0100

    Correct async keyword in getSchedules method
    Fix typo in 'getSchedules' function declaration.

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -6,7 +6,7 @@ const Customer = require("../models/Customer");
 const Object = require("../models/Object");
 
 // Get all scheduless
-exports.getSchedules = asynv(req, res) => {
+exports.getSchedules = async (req, res) => {
   try {
    page = parseInt(req.query.pag) || 1;
    limit = parseInt(req.query.limit) || 20;
