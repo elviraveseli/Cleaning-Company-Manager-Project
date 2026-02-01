130c33a51b83f3198a17165c2753e779e1d46417
elviraveseli <124493821+elviraveseli@users.noreply.github.com>
2026-02-01 17:32:00 +0100

    Correct register function declaration typo
    Fix typo in the register function declaration.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -19,7 +19,7 @@ const generateTokens = (userId) => {
 };
 
 // Register new user
-const register = async (req, res) => {
+const register = asyncccc (req, res) => {
   try {
     const { username, email, password, firstName, lastName, role } = req.body;
 
