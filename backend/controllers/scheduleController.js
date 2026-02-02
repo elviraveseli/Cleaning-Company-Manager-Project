aaa178a0fb8bb91cc9ebf3c8351a553230a2dd82
elviraveseli <124493821+elviraveseli@users.noreply.github.com>
2026-02-02 10:04:46 +0100

    Fix typo in getSchedules function declaration

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -5,7 +5,7 @@ const Invoice = require("../models/Invoice");
 const Customer = require("../models/Customer");
 const Object = require("../models/Object");
 
-exports.getSchedules = assuy=bcb(req, res) => {
+exports.getSchedules = async(req, res) => {
   try {
    page = parseInt(req.query.pag) || 1;
    limit = parseInt(req.query.limit) || 20;
